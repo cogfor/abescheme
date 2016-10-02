6f05233afdae410bc6cb945d63147c57fe8de71c
Rene Nederhand <rene@cogfor.com>
2016-10-02 17:58:30 +0200

    move to installed charm package, cleanup

--- privacy/views.py
+++ privacy/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from privacy.models import Policy, PostedData, Authority
-from Charm.charm.adapters import abenc_adapt_hybrid as charm
-from charm.core.engine.util import objectToBytes,bytesToObject
+from charm.adapters import abenc_adapt_hybrid as charm
+from charm.core.engine.util import objectToBytes, bytesToObject
 from django.core.exceptions import ObjectDoesNotExist
 import form
 import json
@@ -14,172 +14,181 @@ groupObj = charm.PairingGroup('SS512')
 cpabe = charm.CPabe_BSW07(groupObj)
 hyb_abe = charm.HybridABEnc(cpabe, groupObj)
 
+
 def index(request):
-	if request.method == 'POST':
-		p_id = request.POST.get('id', False)
-		if p_id:
-			name = request.POST.get('name', "")
-			try:
-				user = Policy.objects.get(name=name)
-			except ObjectDoesNotExist:
-				user = False
-			if user.name == name:
-				return HttpResponseRedirect(p_id)
-			else:
-				return render(request, 'index.html', {'msg': 'A user with that ID was not found. Please try again.', 'form': form.PolicyForm()})
+    if request.method == 'POST':
+        p_id = request.POST.get('id', False)
+        if p_id:
+            name = request.POST.get('name', "")
+            try:
+                user = Policy.objects.get(name=name)
+            except ObjectDoesNotExist:
+                user = False
+            if user.name == name:
+                return HttpResponseRedirect(p_id)
+            else:
+                return render(request, 'index.html', {'msg': 'A user with that ID was not found. Please try again.',
+                                                      'form': form.PolicyForm()})
 
-		else:
-			name = request.POST.get('id_name', False)
-			view = request.POST.get('viewers', False)
-			displen = request.POST.get('viewlen', False)
-			delete = request.POST.get('deleted', False)
-			datause = request.POST.get('datause', False)
-			tracking = request.POST.get('track', False)
-			gps = request.POST.get('gps', False)
-			if name and view and displen and delete and datause and tracking and gps:
-				policy_str = "("
-				if view == "ADS":
-					policy_str += "(ADS or PVB or ME) and "
-				elif view == "PVB":
-					policy_str += "(PVB or ME) and "
-				else:
-					policy_str += "(ME) and "
-				
-				if displen == "720":
-					policy_str += "(720 or 168 or 24 or 1) and "
-				elif displen == "168":
-					policy_str += "(168 or 24 or 1) and "
-				elif displen == "24":
-					policy_str += "(24 or 1) and "
-				else:
-					policy_str += "(1) and "
+        else:
+            name = request.POST.get('id_name', False)
+            view = request.POST.get('viewers', False)
+            displen = request.POST.get('viewlen', False)
+            delete = request.POST.get('deleted', False)
+            datause = request.POST.get('datause', False)
+            tracking = request.POST.get('track', False)
+            gps = request.POST.get('gps', False)
+            if name and view and displen and delete and datause and tracking and gps:
+                policy_str = "("
+                if view == "ADS":
+                    policy_str += "(ADS or PVB or ME) and "
+                elif view == "PVB":
+                    policy_str += "(PVB or ME) and "
+                else:
+                    policy_str += "(ME) and "
 
-				if datause == "TARGET":
-					policy_str += "(TARGET or STATS or PARTNER or SITE) and "
-				elif datause == "PARTNER":
-					policy_str += "(STATS or PARTNER or SITE) and "
-				elif datause == "STATS":
-					policy_str += "(STATS or SITE) and "
-				else:
-					policy_str += "(SITE) and "
+                if displen == "720":
+                    policy_str += "(720 or 168 or 24 or 1) and "
+                elif displen == "168":
+                    policy_str += "(168 or 24 or 1) and "
+                elif displen == "24":
+                    policy_str += "(24 or 1) and "
+                else:
+                    policy_str += "(1) and "
 
-				if delete == "KEEP":
-					policy_str += "(DELETED OR KEEP) and "
-				else:
-					policy_str += "(KEEP) and "
+                if datause == "TARGET":
+                    policy_str += "(TARGET or STATS or PARTNER or SITE) and "
+                elif datause == "PARTNER":
+                    policy_str += "(STATS or PARTNER or SITE) and "
+                elif datause == "STATS":
+                    policy_str += "(STATS or SITE) and "
+                else:
+                    policy_str += "(SITE) and "
 
-				if tracking == "TRACK":
-					policy_str += "(TRACK or NOTRACK) and "
-				else:
-					policy_str += "(NOTRACK) and "
+                if delete == "KEEP":
+                    policy_str += "(DELETED OR KEEP) and "
+                else:
+                    policy_str += "(KEEP) and "
 
-				if gps == "LOCATE":
-					policy_str += "(LOCATE or NOLOCATE)"
-				else:
-					policy_str += "(NOLOCATE)"
+                if tracking == "TRACK":
+                    policy_str += "(TRACK or NOTRACK) and "
+                else:
+                    policy_str += "(NOTRACK) and "
 
-				policy_str += ")"
-				print "-------------------------------------------------------"
-				print "The user policy is:"
-				print policy_str
+                if gps == "LOCATE":
+                    policy_str += "(LOCATE or NOLOCATE)"
+                else:
+                    policy_str += "(NOLOCATE)"
 
-				p = Policy(name=name, policy=policy_str)
-				p.save()
-				new_user = Policy.objects.get(name=name)
-				p_id = new_user.id
-				return HttpResponseRedirect(str(p_id))
-			else:
-				return render(request, 'index.html', {'msg': 'There was a mistake in your form entries. Please try again.', 'form': form.PolicyForm()})
+                policy_str += ")"
+                print "-------------------------------------------------------"
+                print "The user policy is:"
+                print policy_str
 
-	return render(request, 'index.html', {'form': form.PolicyForm()})
+                p = Policy(name=name, policy=policy_str)
+                p.save()
+                new_user = Policy.objects.get(name=name)
+                p_id = new_user.id
+                return HttpResponseRedirect(str(p_id))
+            else:
+                return render(request, 'index.html',
+                              {'msg': 'There was a mistake in your form entries. Please try again.',
+                               'form': form.PolicyForm()})
+
+    return render(request, 'index.html', {'form': form.PolicyForm()})
+
 
 def policy(request, p_id):
-	user = Policy.objects.get(pk=int(p_id))
-	appname = "privateBook"
-	statuses_decrypted = []
-	if request.method == 'POST':
-		content = request.POST.get('status', False)
-		if content:
-			try:
-				auth = Authority.objects.get(app_name=appname)
-			except ObjectDoesNotExist:
-				auth = False
-			if auth:
-				private = bytesToObject(auth.p_key, groupObj)
-				decoder = bytesToObject(auth.d_key, groupObj)
-				encrypt = hyb_abe.encrypt(private, str(content), str(user.policy))
-				print "-----------------------------------------------------"
-				print "The status update is:"
-				print content
-				print "-----------------------------------------------------"
-				print "Encrypted data, E is:"
-				print encrypt
-				print "--------------------------"
-				print "And it is stored as:"
-				print objectToBytes(encrypt, groupObj)
-				print "-----------------------------------------------------"
-				print "The decryption key is:"
-				print decoder
-				encrypt_b = objectToBytes(encrypt, groupObj)
-				p = PostedData(p_id=int(p_id), status=encrypt_b)
-				p.save()
-			else:
-				return render(request, 'policy.html', {'msg': 'The authority has not created any keys for you to encrypt your data!', 'id': p_id, 'name': user.name})
-			return HttpResponseRedirect('')
-		else:
-			return render(request, 'policy.html', {'msg': 'You didn\'t enter a status. Please try again.', 'id': p_id, 'name': user.name})
+    user = Policy.objects.get(pk=int(p_id))
+    appname = "privateBook"
+    statuses_decrypted = []
+    if request.method == 'POST':
+        content = request.POST.get('status', False)
+        if content:
+            try:
+                auth = Authority.objects.get(app_name=appname)
+            except ObjectDoesNotExist:
+                auth = False
+            if auth:
+                private = bytesToObject(auth.p_key, groupObj)
+                decoder = bytesToObject(auth.d_key, groupObj)
+                encrypt = hyb_abe.encrypt(private, str(content), str(user.policy))
+                print "-----------------------------------------------------"
+                print "The status update is:"
+                print content
+                print "-----------------------------------------------------"
+                print "Encrypted data, E is:"
+                print encrypt
+                print "--------------------------"
+                print "And it is stored as:"
+                print objectToBytes(encrypt, groupObj)
+                print "-----------------------------------------------------"
+                print "The decryption key is:"
+                print decoder
+                encrypt_b = objectToBytes(encrypt, groupObj)
+                p = PostedData(p_id=int(p_id), status=encrypt_b)
+                p.save()
+            else:
+                return render(request, 'policy.html',
+                              {'msg': 'The authority has not created any keys for you to encrypt your data!',
+                               'id': p_id, 'name': user.name})
+            return HttpResponseRedirect('')
+        else:
+            return render(request, 'policy.html',
+                          {'msg': 'You didn\'t enter a status. Please try again.', 'id': p_id, 'name': user.name})
 
-	try:
-		auth = Authority.objects.get(app_name=appname)
-	except ObjectDoesNotExist:
-		auth = False
-	if auth:
-		statuses = PostedData.objects.filter(p_id=int(p_id))
-		for s in statuses:
-			status_cipher = bytesToObject(s.status, groupObj)
-			private = bytesToObject(auth.p_key, groupObj)
-			decoder = bytesToObject(auth.d_key, groupObj)
-			try:
-				status_pair = (hyb_abe.decrypt(private, decoder, status_cipher), s.posted)
-			except:
-				status_pair = ("Your status could not be displayed: this service does not support your privacy policy!", s.posted)
-			statuses_decrypted.append(status_pair)
-	if statuses_decrypted:
-		context = {'statuses': statuses_decrypted, 'id': p_id, 'name': user.name}
-	else:
-		context = {'id': p_id, 'name': user.name}		
-	return render(request, 'policy.html', context)
+    try:
+        auth = Authority.objects.get(app_name=appname)
+    except ObjectDoesNotExist:
+        auth = False
+    if auth:
+        statuses = PostedData.objects.filter(p_id=int(p_id))
+        for s in statuses:
+            status_cipher = bytesToObject(s.status, groupObj)
+            private = bytesToObject(auth.p_key, groupObj)
+            decoder = bytesToObject(auth.d_key, groupObj)
+            try:
+                status_pair = (hyb_abe.decrypt(private, decoder, status_cipher), s.posted)
+            except:
+                status_pair = (
+                "Your status could not be displayed: this service does not support your privacy policy!", s.posted)
+            statuses_decrypted.append(status_pair)
+    if statuses_decrypted:
+        context = {'statuses': statuses_decrypted, 'id': p_id, 'name': user.name}
+    else:
+        context = {'id': p_id, 'name': user.name}
+    return render(request, 'policy.html', context)
+
 
 def authority(request):
-	if (request.is_ajax()):
-		name = request.GET.get('name', False)
-		attr_list = request.GET.getlist('list', False)
-		attr_list = [x.encode('UTF8') for x in attr_list]
-		print name, attr_list
-		if name and attr_list:
-			(private, master) = hyb_abe.setup()
-			decoder = hyb_abe.keygen(private, master, attr_list)
-			print "---------------------------------------------"
-			print "The Attribute List is:"
-			print attr_list
-			print "---------------------------------------------"
-			print "The public parameters PK are:"
-			print private
-			print "---------------------------------------------"
-			print "The private key D is:"
-			print decoder
-			decoder_b = objectToBytes(decoder, groupObj)
-			private_b = objectToBytes(private, groupObj)
-			try:
-				a = Authority.objects.get(app_name=name)
-			except ObjectDoesNotExist:
-				a = Authority(app_name=name, attr_list=attr_list, p_key=private_b, d_key=decoder_b)
-				a.save()
-			json_res = {'msg': 'Success!'}
-		else:
-			json_res = {'msg': 'Failure.'}
+    if (request.is_ajax()):
+        name = request.GET.get('name', False)
+        attr_list = request.GET.getlist('list', False)
+        attr_list = [x.encode('UTF8') for x in attr_list]
+        print name, attr_list
+        if name and attr_list:
+            (private, master) = hyb_abe.setup()
+            decoder = hyb_abe.keygen(private, master, attr_list)
+            print "---------------------------------------------"
+            print "The Attribute List is:"
+            print attr_list
+            print "---------------------------------------------"
+            print "The public parameters PK are:"
+            print private
+            print "---------------------------------------------"
+            print "The private key D is:"
+            print decoder
+            decoder_b = objectToBytes(decoder, groupObj)
+            private_b = objectToBytes(private, groupObj)
+            try:
+                a = Authority.objects.get(app_name=name)
+            except ObjectDoesNotExist:
+                a = Authority(app_name=name, attr_list=attr_list, p_key=private_b, d_key=decoder_b)
+                a.save()
+            json_res = {'msg': 'Success!'}
+        else:
+            json_res = {'msg': 'Failure.'}
 
-		return HttpResponse(json.dumps(json_res), content_type='application/json')
+        return HttpResponse(json.dumps(json_res), content_type='application/json')
 
-	return render(request, 'authority.html');
-
+    return render(request, 'authority.html');
